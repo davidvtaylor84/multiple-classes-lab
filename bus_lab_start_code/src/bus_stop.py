bcdb4ed0bbe874e4c087ed7afecd458cdf5004e4
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-02 16:16:38 +0100

    completed lab work

---
+++ bus_lab_start_code/src/bus_stop.py
@@ -0,0 +1,15 @@
+class BusStop:
+    def __init__(self, name):
+        self.name = name
+        self.queue = []
+
+    def add_to_queue(self, passenger):
+        self.queue.append(passenger.name)
+
+    def queue_length(self):
+        return len(self.queue)
+
+    def clear(self):
+        self.queue.clear()
+    
+
